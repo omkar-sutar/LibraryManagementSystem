c6fad58eca003477fe65452094ba7cb2587a7ca9
amanpandit <60512626+amanpanditap@users.noreply.github.com>
2022-05-10 19:25:38 +0530

    test

--- library/models.py
+++ library/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=200, blank=True)
+    avatar = models.ImageField(default = 'avatar.png', upload_to='avatars/')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
