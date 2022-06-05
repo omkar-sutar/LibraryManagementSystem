3d6939be1353da645a64ab31b3d8381764e5ad2c
omkar-sutar <omkartsutar2001@gmail.com>
2022-06-05 23:24:59 +0530

    modified patterns

--- library/urls.py
+++ library/urls.py
@@ -8,10 +8,12 @@ urlpatterns = [
     path('', views.landingpage, name='landingPage'),
     path('home/', views.homePage, name='homePage'),
     path('team/', views.teamPage, name='team'),
-    path('login', views.Login.as_view(), name='Login'),
-    path('register', views.register.as_view(), name='register'),
+    path('login', views.Login.as_view(), name='login'),
+    path('register', views.Register.as_view(), name='register'),
     path('profile/<str:username>', views.viewProfile.as_view(), name='viewProfile'),
     path('resetPassword', views.resetPassword.as_view(), name='resetPassword'),
     path('logout', views.Logout, name='Logout'),
     path('uploadbarcode', views.uploadBarcode.as_view(),name="uploadBarcode"),
+    path('search/<str:query>',views.search,name='search'),
+
 ]
